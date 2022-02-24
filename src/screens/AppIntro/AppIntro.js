9749fa681c0c015566c19d919f26affdbcae0b83
ahmedfouad01099 <ahmedfouad01099#gmail.com>
2022-02-24 12:20:30 +0200

    adding screens

---
+++ src/screens/AppIntro/AppIntro.js
@@ -0,0 +1,66 @@
+import React, {useEffect, useState} from 'react';
+import Onboarding from '../../components/Onboarding/Onboarding';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {ActivityIndicator, StyleSheet, View} from 'react-native';
+import {useDispatch, useSelector} from 'react-redux';
+import Login from '../Login/Login';
+import Navigation from '../../Navigation/Navigation';
+
+const Loading = () => {
+  return (
+    <View>
+      <ActivityIndicator size={'large'} />
+    </View>
+  );
+};
+function AppIntro(props) {
+  const [loading, setLoading] = useState(true);
+  const [viewOnboarding, setViewOnboarding] = useState(false);
+
+  const checkOnboarding = async () => {
+    try {
+      const value = await AsyncStorage.getItem('@viewOnboarding');
+
+      if (value !== null) {
+        setViewOnboarding(true);
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    checkOnboarding();
+  }, []);
+
+  // const token = useSelector(state => state.login.token);
+  
+  return (
+    <View style={styles.container}>
+      {loading ? (
+        <Loading />
+      ) : viewOnboarding ? (
+        true ? (
+          <View style={{width: '100%', flex: 1}}>
+            <Navigation {...props} />
+          </View>
+        ) : (
+          <Login {...props} />
+        )
+      ) : (
+        <Onboarding {...props} />
+      )}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+export default AppIntro;
---
+++ src/screens/Login/Login.js
@@ -0,0 +1,100 @@
+import React from 'react';
+import {Image, StyleSheet, Text, TextInput, View} from 'react-native';
+import MainButton from '../../components/Button/MainButton';
+import InputText from '../../components/Inputs/InputText';
+// import AntDesign from 'react-native-vector-icons/AntDesign';
+import {GoogleSvg} from '../../constants/Icons';
+import {useDispatch, useSelector} from 'react-redux';
+// import {onChangeLoginInput, onLoginHandler} from '../../store/Login/Login';
+import Spinner from '../../components/Spinner/Spinner';
+
+function Login(props) {
+  const dispatch = useDispatch();
+  // const {loginForm, loading} = useSelector(state => state.login);
+  return (
+    <View style={styles.container}>
+      <Image source={require('../../../assets/images/1.png')} />
+      <Text style={{fontSize: 20, fontWeight: 'bold', marginTop: 30}}>
+        Welcome Back!
+      </Text>
+      <Text style={{fontSize: 15, marginBottom: 20}}>
+        Login To Your Account
+      </Text>
+
+      <InputText
+        styleProp={styles.TxtInput}
+        placeholder={'user name'}
+        imageSource={require('../../../assets/images/user.png')}
+        name={'userName'}
+        borderColor={'#48B8AC'}
+        // onChange={e => dispatch(onChangeLoginInput(e, 'email'))}
+        // value={loginForm.email.value}
+      />
+      <InputText
+        styleProp={styles.TxtInput}
+        placeholder={'password'}
+        imageSource={require('../../../assets/images/padlock.png')}
+        secureTextEntry={true}
+        name={'pass'}
+        borderColor={'#48B8AC'}
+        // onChange={e => dispatch(onChangeLoginInput(e, 'password'))}
+        // value={loginForm.password.value}
+      />
+
+      <MainButton
+        title={'Log in'}
+        // loading={loading}
+        btnBackgroundColor={'#222D58'}
+        txtColor={'#fff'}
+        props={props}
+        // onPress={() => {
+        //   dispatch(
+        //     onLoginHandler(
+        //       loginForm.email.value,
+        //       loginForm.password.value,
+        //       props,
+        //     ),
+        //   );
+        //   // props.navigation.navigate('home');
+        // }}
+      />
+      {/* <Text style={{color: '#48B8AC'}}>or connect using</Text> */}
+      {/* <MainButton
+        title={'Google'}
+        btnBackgroundColor={'#F14336'}
+        txtColor={'#fff'}
+        btnIcon={
+          <AntDesign
+            name="google"
+            size={20}
+            color={'#fff'}
+            style={styles.googleIcon}
+          />
+        }
+      /> */}
+    </View>
+  );
+}
+
+export default Login;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  TxtInput: {
+    borderColor: '#707070',
+    width: '80%',
+    borderWidth: 1,
+    padding: 10,
+    paddingVertical: 13,
+    borderRadius: 4,
+    marginTop: 9,
+    paddingLeft: 25,
+  },
+  googleIcon: {
+    marginRight: 15,
+  },
+});
